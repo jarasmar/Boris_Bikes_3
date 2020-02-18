e59500aa18192c5860ff45ccf50845a4655ee618
Student <student@Makerss-MacBook-Air.local>
2020-02-18 16:45:17 +0000

    Added test: DockingStation responds to release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
 
+  #it "Responds to the method release_bike" do
+  #  is_expected.to respond_to(:release_bike)
+  #end
+  it { is_expected.to respond_to(:release_bike) }
 end
